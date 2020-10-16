5690c49c86cb48795c1c61597daa6eb8af353c03
Jack <joaco349@gmail.com>
2020-10-16 09:36:20 -0500

    Otro mas

---
+++ saludo.py
@@ -0,0 +1,6 @@
+
+
+nombre = input('Ingresa tu nombre: ')
+edad= int(input('Ingresa tu edad: '))
+Saludo = f'Hola {nombre}, tu edad es {edad} años.'
+print(Saludo + ' Esto es una cadena de texto y tiene una longitud de', len(Saludo),'caracteres')
